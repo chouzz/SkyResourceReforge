29dedcadc8a6d9077160e4acc313ecc5cf7f1ee6
chouzz <zhouhua258@outlook.com>
2026-01-20 22:25:12 +0800

    feat: expand datagen translations and language provider
    implementation: Expanded language translations from original mod.
    
    Changes:
    - Updated ModLanguageProvider with comprehensive translations
    - Aligned item/block names to match original mod's naming conventions
    - Added "Cutting" prefix to knife names (Cactus Cutting Knife, etc.)
    - Updated block names (Hardened Coal Block, Smart Combustion Controller, etc.)
    - Ran datagen to regenerate en_us.json with new translations
    
    Datagen now properly generates:
    - Block states for all blocks
    - Item models for all items
    - Language file with translations from original mod

--- src/main/java/com/chouzz/skyresourcereforge/datagen/ModLanguageProvider.java
+++ src/main/java/com/chouzz/skyresourcereforge/datagen/ModLanguageProvider.java
@@ -14,41 +14,52 @@ public class ModLanguageProvider extends LanguageProvider {
     @Override
     protected void addTranslations() {
         add("itemGroup.skyresourcereforge.main", "SkyResource Reforge");
-        
-        addItem(ModItems.CACTUS_KNIFE, "Cactus Knife");
-        addItem(ModItems.STONE_KNIFE, "Stone Knife");
-        addItem(ModItems.IRON_KNIFE, "Iron Knife");
-        addItem(ModItems.DIAMOND_KNIFE, "Diamond Knife");
-        addItem(ModItems.STONE_GRINDER, "Stone Grinder");
-        addItem(ModItems.IRON_GRINDER, "Iron Grinder");
-        addItem(ModItems.DIAMOND_GRINDER, "Diamond Grinder");
+
+        // Knives
+        addItem(ModItems.CACTUS_KNIFE, "Cactus Cutting Knife");
+        addItem(ModItems.STONE_KNIFE, "Stone Cutting Knife");
+        addItem(ModItems.IRON_KNIFE, "Iron Cutting Knife");
+        addItem(ModItems.DIAMOND_KNIFE, "Diamond Cutting Knife");
+
+        // Grinders
+        addItem(ModItems.STONE_GRINDER, "Stone Rock Grinder");
+        addItem(ModItems.IRON_GRINDER, "Iron Rock Grinder");
+        addItem(ModItems.DIAMOND_GRINDER, "Diamond Rock Grinder");
+
+        // Misc Items
         addItem(ModItems.WATER_EXTRACTOR, "Water Extractor");
-        
-        addBlock(ModBlocks.COMPRESSED_COAL_BLOCK, "Compressed Coal Block");
-        addBlock(ModBlocks.COMBUSTION_CONTROLLER, "Combustion Controller");
+        addItem(ModItems.CACTUS_FRUIT, "Cactus Fruit");
+        addItem(ModItems.HEAVY_SNOWBALL, "Heavy Snowball");
+        addItem(ModItems.HEAVY_EXPLOSIVE_SNOWBALL, "Explosive Heavy Snowball");
+        addItem(ModItems.FLESHY_SNOW_NUGGET, "Fleshy Snow Nugget");
+        addItem(ModItems.BASE_COMPONENT, "Base Component");
+        addItem(ModItems.TECH_COMPONENT, "Tech Component");
+
+        // Blocks
+        addBlock(ModBlocks.COMPRESSED_COAL_BLOCK, "Hardened Coal Block");
+        addBlock(ModBlocks.COMBUSTION_CONTROLLER, "Smart Combustion Controller");
         addBlock(ModBlocks.COMBUSTION_COLLECTOR, "Combustion Collector");
         addBlock(ModBlocks.ROCK_CRUSHER, "Rock Crusher");
         addBlock(ModBlocks.ROCK_CLEANER, "Rock Cleaner");
         addBlock(ModBlocks.CASING, "Casing");
         addBlock(ModBlocks.SANDY_NETHERRACK, "Sandy Netherrack");
-        addBlock(ModBlocks.COAL_INFUSED_BLOCK, "Coal Infused Block");
+        addBlock(ModBlocks.COAL_INFUSED_BLOCK, "Alchemical Coal Block");
         addBlock(ModBlocks.DARK_MATTER_BLOCK, "Dark Matter Block");
         addBlock(ModBlocks.LIGHT_MATTER_BLOCK, "Light Matter Block");
         addBlock(ModBlocks.BLAZE_POWDER_BLOCK, "Blaze Powder Block");
         addBlock(ModBlocks.MAGMAFIED_STONE, "Magmafied Stone");
-        addBlock(ModBlocks.DRY_CACTUS, "Dry Cactus");
-        addBlock(ModBlocks.CACTUS_FRUIT_NEEDLE, "Cactus Fruit Needle");
+        addBlock(ModBlocks.DRY_CACTUS, "Dehydrated Cactus");
+        addBlock(ModBlocks.CACTUS_FRUIT_NEEDLE, "Cactus Fruit On A Needle");
         addBlock(ModBlocks.DIRT_FURNACE, "Dirt Furnace");
         addBlock(ModBlocks.MINI_FREEZER, "Mini Freezer");
         addBlock(ModBlocks.IRON_FREEZER, "Iron Freezer");
-        addBlock(ModBlocks.LIGHT_FREEZER, "Light Freezer");
+        addBlock(ModBlocks.LIGHT_FREEZER, "Light Matter Freezer");
         addBlock(ModBlocks.AQUEOUS_CONCENTRATOR, "Aqueous Concentrator");
         addBlock(ModBlocks.AQUEOUS_DECONCENTRATOR, "Aqueous Deconcentrator");
         addBlock(ModBlocks.HEAVY_SNOW, "Heavy Snow");
-        addItem(ModItems.CACTUS_FRUIT, "Cactus Fruit");
-        addItem(ModItems.HEAVY_SNOWBALL, "Heavy Snowball");
-        addItem(ModItems.HEAVY_EXPLOSIVE_SNOWBALL, "Heavy Explosive Snowball");
-        addItem(ModItems.BASE_COMPONENT, "Base Component");
-        addItem(ModItems.TECH_COMPONENT, "Tech Component");
+        addBlock(ModBlocks.PETRIFIED_WOOD, "Petrified Wood");
+        addBlock(ModBlocks.PETRIFIED_PLANKS, "Petrified Wood Planks");
+        addBlock(ModBlocks.SILVERFISH_DISRUPTOR, "Lepisma Saccharina Teleportation Disruptor");
+        addBlock(ModBlocks.CRUCIBLE, "Crucible");
     }
 }
