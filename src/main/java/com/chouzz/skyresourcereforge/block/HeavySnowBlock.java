cdf9d00c58aabd24b5d7b2d7188c2b64254b634d
chouzz <zhouhua258@outlook.com>
2026-01-20 12:38:26 +0800

    fix: use BlockBehaviour.Properties in HeavySnowBlock constructor

--- src/main/java/com/chouzz/skyresourcereforge/block/HeavySnowBlock.java
+++ src/main/java/com/chouzz/skyresourcereforge/block/HeavySnowBlock.java
@@ -1,10 +1,9 @@
 package com.chouzz.skyresourcereforge.block;
 
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 
 public class HeavySnowBlock extends BaseBlock {
-    public HeavySnowBlock(Properties properties) {
+    public HeavySnowBlock(BlockBehaviour.Properties properties) {
         super(properties);
     }
 }
